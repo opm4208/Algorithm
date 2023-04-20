062185069c4453eb82b4ed89b859e6494af91d6b
민 박 <opm4208@gmail.com>
2023-04-20 21:10:47 +0900

    feat: 리스트, 연결리스트 반복기 구현
    foreach에서 반복 확인

---
+++ 03._Iterator/Program.cs
@@ -0,0 +1,28 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace _03._Iterator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Iterator.List<int> list = new Iterator.List<int>();
+            Iterator.LinkedList<int> llist = new Iterator.LinkedList<int>();
+            for (int i = 0; i < 5; i++)
+            {
+                list.Add(i);
+                llist.AddLast(i);
+            }
+
+            foreach (int i in list)
+            {
+                Console.WriteLine(i);
+            }
+
+            foreach (int i in llist)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
